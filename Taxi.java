f53ba21e6577a77fa880b77b260150ef2e50c127
Monicatatiana11 <58921913+Monicatatiana11@users.noreply.github.com>
2021-03-03 21:52:15 -0500

    Cambio 1

---
+++ Taxi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taxi;
+
+/**
+ *
+ * @author CAMILO CORDOBA
+ */
+public class Taxi {
+
+ private String matricula;
+ private int    modelo;
+ private String potencia;
+ 
+ 
+ // inicialisacion de la clase con constructor
+ public Taxi(String matricula,int modelo,String potencia){
+ 
+ this.matricula=matricula;
+ this.modelo=modelo;
+ this.potencia=potencia;
+
+ }   
+
+}
