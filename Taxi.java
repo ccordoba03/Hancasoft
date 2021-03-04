1630817b4bd11d0d3e9bdd597e06d2b37c3aa1c3
ccordoba03 <58755480+ccordoba03@users.noreply.github.com>
2021-03-03 21:55:48 -0500

    Update Taxi.java
    Se generan metodos set and get

--- Taxi.java
+++ Taxi.java
@@ -16,13 +16,32 @@ public class Taxi {
  private String potencia;
  
  
- // inicialisacion de la clase con constructor
+ // inicialisacion de la clase con constructor-monica
  public Taxi(String matricula,int modelo,String potencia){
  
  this.matricula=matricula;
  this.modelo=modelo;
  this.potencia=potencia;
 
+  
+ // metodos get and set-camilo
+public void setMatricula(
+String matricula){
+
+  this.matricula=matricula;
+    
+}
+
+public void setModelo(int modelo){
+this.modelo=modelo;
+}
+
+public void setPotencia(String potencia){
+this.potencia=potencia;
+}
+ 
+  
+  
  }   
 
 }
